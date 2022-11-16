cd8837a80d5566d5c4599d405d917dbf6ede5afb
Cesar Lucumi <clucumi.94@gmail.com>
2022-11-15 22:45:34 -0500

    ES11 Module Learned

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 89598491256489631213n;
+const anotherBigNumber = BigInt(89598491256489631213);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
---
+++ src/es11/04-globalThis.js
@@ -0,0 +1,4 @@
+console.log(window); //navegador
+console.log(global); //node
+console.log(selft); //webworer
+console.log(globalThis); // ALl of them
